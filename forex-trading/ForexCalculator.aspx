336f9b3177e37e1428e298ec98253244e52ad273
roystonraphael <royston.raphael@gmail.com>
2015-06-14 20:40:24 +1200

    New changes made

---
+++ forex-trading/ForexCalculator.aspx
@@ -0,0 +1,140 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ForexCalculator.aspx.cs" Inherits="forex_trading.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript">
+        function displayForm() {
+            removeBoxes();
+            var numberOfTextBoxes = document.getElementById('<%=txtSplits.ClientID%>').value;
+
+            for (i = 0; i < numberOfTextBoxes; i++) {
+                var newInput = document.createElement("input");
+                newInput.setAttribute("type", "Text");
+                newInput.setAttribute("name", "txtSplitPercentage" + i);
+                document.getElementById("divToAddSplitPercent").appendChild(newInput);
+            }           
+        }
+
+        function removeBoxes() {
+            var d = document.getElementById("divToAddSplitPercent");
+            while (d.firstChild) d.removeChild(d.firstChild);
+        }
+
+        function displayBoxes() {
+            var numberOfTextBoxes = document.getElementById('<%=txtSplits.ClientID%>').value;
+
+            for (i = 0; i < numberOfTextBoxes; i++) {
+                var newInput = document.createElement("input");
+                newInput.setAttribute("type", "Text");
+                newInput.setAttribute("name", "txtSplitPercentage" + i);
+                document.getElementById("divToAddSplitPercent").appendChild(newInput);
+            }
+        }
+    </script>
+</head>
+
+<body onload="">
+    <form id="form1" runat="server">
+    <div>Test page</div>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblLot" runat="server" Text="Lot size:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtLotSize" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblPrice" runat="server" Text="Price to buy/ sell:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPrice" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblLoss" runat="server" Text="Loss acceptable:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtLoss" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblProfit" runat="server" Text="Profit required:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtProfit" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblSplits" runat="server" Text="Number of Splits:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtSplits" runat="server" Text="2" onBlur="displayForm()"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblTradeType" runat="server" Text="Trade Type:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddlTradeType" runat="server">
+                            <asp:ListItem Text="Buy" Value="Buy" Selected="True"></asp:ListItem>
+                            <asp:ListItem Text="Sell" Value="Sell"></asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblSplitPercent" runat="server" Text="Split percentage:"></asp:Label>
+                    </td>
+                    <td id="divToAddSplitPercent">
+                        <asp:PlaceHolder ID="plcSplitPercentage" runat="server" />  
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnCalculate" runat="server" Text="Calculate" OnClick="btnCalculate_Click" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <br />
+        <asp:Table ID="tblResult" runat="server" GridLines="Both" >
+            <asp:TableHeaderRow ID="Table1HeaderRow"
+                BackColor="LightBlue"
+                runat="server">
+                <asp:TableHeaderCell
+                    Scope="Column"
+                    Text="Price Value" />
+                <asp:TableHeaderCell
+                    Scope="Column"
+                    Text="Pip Value SP" />
+                <asp:TableHeaderCell
+                    Scope="Column"
+                    Text="Stop Loss" />
+                <asp:TableHeaderCell
+                    Scope="Column"
+                    Text="Stop Loss Value" />
+                <asp:TableHeaderCell
+                    Scope="Column"
+                    Text="Pip Value TP" />
+                <asp:TableHeaderCell
+                    Scope="Column"
+                    Text="Take Profit" />
+                <asp:TableHeaderCell
+                    Scope="Column"
+                    Text="Take Profit Value" />
+            </asp:TableHeaderRow>  
+        </asp:Table>
+    </form>
+</body>
+</html>
---
+++ forex-trading/ForexCalculator.aspx.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace forex_trading
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        double splitPercent, lossAcceptable, profitRequired, lotSize, priceToBuyOrSell = 0;
+        bool isFirst = false;
+        const string VIEW_STATE_NAME = "PercentageValues";
+        const string CONTROL_NAME = "txtSplitPercentage";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //txtSplits.Attributes.Add("OnKeyPress", "displayForm()");
+
+            if (Page.IsPostBack)
+            {
+                //displayInputControls();
+                //ViewState("PercentageSplit") = splitPercent;
+            }
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            saveInputControls(Int32.Parse(txtSplits.Text));
+            generateValues();
+        }
+
+        private void saveInputControls(int noOfSpilts)
+        {
+            if (ViewState[VIEW_STATE_NAME] == null)
+            {
+                for (int i = 0; i < noOfSpilts; i++)
+                {
+                    TextBox txtSplitPercent = new TextBox();
+                    double percent = 0;
+                    Double.TryParse(Request.Form[CONTROL_NAME + i.ToString()], out percent);
+                    txtSplitPercent.Text = percent.ToString();
+                    txtSplitPercent.ID = CONTROL_NAME + i;
+                    plcSplitPercentage.Controls.Add(txtSplitPercent);
+                    isFirst = true;
+                    //displayInputControls();
+                }
+            }           
+        }
+
+        private void displayInputControls()
+        {
+            if (ViewState[VIEW_STATE_NAME] != null && !isFirst)
+            {
+                List<double> splitValues = (List<double>)ViewState[VIEW_STATE_NAME];
+                int i = 0;
+                foreach (double eachValue in splitValues)
+                {
+                    TextBox txtSplitPercent = new TextBox();
+                    txtSplitPercent.ID = CONTROL_NAME + i.ToString();
+                    txtSplitPercent.Text = eachValue.ToString();
+                    plcSplitPercentage.Controls.Add(txtSplitPercent);
+                    i++;
+                }
+            }
+        }
+
+
+        private void generateValues()
+        {
+            Double.TryParse(txtProfit.Text, out profitRequired);
+            Double.TryParse(txtLoss.Text, out lossAcceptable);
+            Double.TryParse(txtLotSize.Text, out lotSize);
+            Double.TryParse(txtPrice.Text, out priceToBuyOrSell);
+
+            getSplitTextBoxData(Convert.ToInt32(txtSplits.Text));
+
+            displayInputControls();
+        }
+
+        private void getSplitTextBoxData(int noOfSpilts)
+        {
+            List<Double> listOfPercentages = new List<Double>();
+            for (int i = 0; i < noOfSpilts; i++)
+            {
+                string nameOfSplitBox = CONTROL_NAME + i.ToString();
+
+                if (ViewState[VIEW_STATE_NAME] != null)
+                {
+                    TextBox txtPercentage = (TextBox)plcSplitPercentage.FindControl(nameOfSplitBox);
+                }
+
+                Double.TryParse(Request.Form[nameOfSplitBox], out splitPercent);
+                //Response.Write("Test " + splitPercent.ToString());
+                listOfPercentages.Add(splitPercent);
+                //adding rows to table based of calcs
+                addTableRows();
+            }
+            ViewState.Add(VIEW_STATE_NAME, listOfPercentages);
+        }
+
+        private void addTableRows()
+        {
+            TableRow tableResultsRow = new TableRow();
+            tblResult.Rows.Add(tableResultsRow);
+
+            TableCell tableResultsCellPrice = new TableCell();
+            tableResultsCellPrice.Text = txtPrice.Text;
+            tableResultsRow.Cells.Add(tableResultsCellPrice);
+
+            TableCell tableResultsCellStopLossPip = new TableCell();
+            tableResultsCellStopLossPip.Text = calculatePipStopLoss();
+            tableResultsRow.Cells.Add(tableResultsCellStopLossPip);
+
+            TableCell tableResultsCellStopLoss = new TableCell();
+            tableResultsCellStopLoss.Text = calculateStopLoss();
+            tableResultsRow.Cells.Add(tableResultsCellStopLoss);
+
+            TableCell tableResultsCellStopLossValue = new TableCell();
+            tableResultsCellStopLossValue.Text = calculateStopLossValue();
+            tableResultsRow.Cells.Add(tableResultsCellStopLossValue);
+
+            TableCell tableResultsCellTakeProfitPip = new TableCell();
+            tableResultsCellTakeProfitPip.Text = calculatePipTakeProfit();
+            tableResultsRow.Cells.Add(tableResultsCellTakeProfitPip);
+
+            TableCell tableResultsCellTakeProfit = new TableCell();
+            tableResultsCellTakeProfit.Text = calculateTakeProfit();
+            tableResultsRow.Cells.Add(tableResultsCellTakeProfit);
+
+            TableCell tableResultsCellTakeProfitValue = new TableCell();
+            tableResultsCellTakeProfitValue.Text = calculateTakeProfitValue();
+            tableResultsRow.Cells.Add(tableResultsCellTakeProfitValue);
+        }
+
+        private string calculatePipStopLoss()
+        {
+            double stopLossPip = (splitPercent / 100) * lossAcceptable;
+            stopLossPip = stopLossPip / lotSize;
+            return stopLossPip.ToString();
+        }
+
+        private string calculateStopLossValue()
+        {
+            double stopLossValue = (splitPercent / 100) * lossAcceptable;
+            return stopLossValue.ToString();
+        }
+
+        private string calculateTakeProfitValue()
+        {
+            double takeProfitValue = (splitPercent / 100) * profitRequired;
+            return takeProfitValue.ToString();
+        }
+
+        private string calculatePipTakeProfit()
+        {
+            double takeProfitPip = (splitPercent / 100) * profitRequired;
+            takeProfitPip = takeProfitPip / lotSize;
+            return takeProfitPip.ToString();
+        }
+
+        private string calculateStopLoss()
+        {
+            double stopLoss = (splitPercent / 100) * lossAcceptable;
+            stopLoss = stopLoss / lotSize;
+            if (ddlTradeType.SelectedValue == "Buy")
+            {
+                stopLoss = priceToBuyOrSell - (stopLoss * 0.00001);
+            }
+            else
+            {
+                stopLoss = priceToBuyOrSell + (stopLoss * 0.00001);
+            }
+            stopLoss = Math.Round(stopLoss, 5);
+            //we need to know pip value for the currency pair
+            // EUR/ USD = 0.0001
+            // GBP/USD = 0.0001
+            // USD/ JPY = 0.01
+            // USD/ CHF = 0.0001
+            // NZD/ USD = 0.0001
+            // USD/ CAD = 0.0001
+            return String.Format("{0:0.00000}", stopLoss);
+        }
+
+        private string calculateTakeProfit()
+        {
+            double takeProfit = (splitPercent / 100) * profitRequired;
+            takeProfit = takeProfit / lotSize;
+
+            if (ddlTradeType.SelectedValue == "Buy")
+            {
+                takeProfit = priceToBuyOrSell + (takeProfit * 0.00001);
+            }
+            else
+            {
+                takeProfit = priceToBuyOrSell - (takeProfit * 0.00001);
+            }
+            takeProfit = Math.Round(takeProfit, 5);
+
+            return String.Format("{0:0.00000}", takeProfit);
+        }
+    }
+}
